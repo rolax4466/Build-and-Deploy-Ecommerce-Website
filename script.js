f93191b6a1febece01e64842edc5dfcdd6dc6c88
rolax <rolaxcheruiyot325@gmail.com>
2024-02-07 01:46:36 +0300

    cara fashion shop

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Script for navigation bar
+// Script for navigation
+// Get references to DOM elements
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+// Add event listener to toggle navigation menu
+if (bar ) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+// Add event listener to close navigation menu
+if (close ) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+
+  
